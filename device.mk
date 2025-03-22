a3fcd6416e07e4478c684fb1dd2c54955a139828
kazusakiii27 <184460707+kazusakiii27@users.noreply.github.com>
2025-03-22 11:41:06 +0700

    blossom

--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/xiaomi/dandelion
+DEVICE_PATH := device/xiaomi/blossom
 
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
